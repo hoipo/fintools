b9d76221a8c0b16651275d77d40a56422e193279
hoipo <578344975@qq.com>
2020-12-05 22:48:42 +0800

    fix bug

--- app/__init__.py
+++ app/__init__.py
@@ -8,14 +8,14 @@ from flask_migrate import Migrate
 app = Flask(__name__, static_folder='../static', static_url_path='')
 
 
-
+ENV = ""
 
 if ENV == "debug":
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://postgres:123456@localhost:5432/postgres'
     app.debug = True
 else:
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://qkhusxirwrxtnc:3343256fc71c5934628acfd574b43d375774b169972a6704088ae09f731996b1@ec2-3-213-192-58.compute-1.amazonaws.com:5432/d302ufp80rp2vq'
-     app.debug = False
+    app.debug = False
     
 
 @app.route('/')
