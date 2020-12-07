66652cde511de5bbf59536d937adba6e0f88dd22
hoipo <578344975@qq.com>
2020-12-07 08:56:06 +0800

    add new feature at front-end

--- app/handler.py
+++ app/handler.py
@@ -32,8 +32,7 @@ def get_live_data_of_ag(no_cache=False):
         ag = Ag.query.order_by(Ag.id.desc()).first()
         return {
             'date': ag.date,
-            # 'time': ag.create_time.strftime('%H:%M:%S'),
-            'time': '15:00:00',
+            'time': ag.create_time.strftime('%H:%M:%S'),
             'ag_future_price': ag.ag_future_price,
             'ag_future_averge_price': ag.ag_future_averge_price,
             'ag_future_previous_settlement_price': ag.ag_future_previous_settlement_price,
