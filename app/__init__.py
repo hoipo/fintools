609fb537bd86589e79e145db3e91bb378c69c704
hoipo <578344975@qq.com>
2020-12-31 11:36:50 +0800

    隐藏mongodbURL

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os
 from flask_restful import Api, Resource
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -12,12 +13,11 @@ if ENV == "debug":
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://postgres:123456@localhost:5432/postgres'
     app.debug = True
 else:
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://qkhusxirwrxtnc:3343256fc71c5934628acfd574b43d375774b169972a6704088ae09f731996b1@ec2-3-213-192-58.compute-1.amazonaws.com:5432/d302ufp80rp2vq'
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
     # app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root1234@154.8.158.147:3306/ag'
     app.debug = False
 
-mongo_ag_tick = pymongo.MongoClient(
-"mongodb://root:root1234@119.27.188.244:27017/?authSource=admin")["ag"]["ag_tick"]
+mongo_ag_tick = pymongo.MongoClient(os.getenv('MONGODB_URL'))["ag"]["ag_tick"]
 
 @ app.route('/')
 def index():
